08d6be99844f827e9abbaa0e2443979fa187279a
Thomascogez <thomas.cogez@gmail.com>
2024-04-07 22:45:36 +0200

    feat: add get_ws_url method

--- src/browser/mod.rs
+++ src/browser/mod.rs
@@ -178,6 +178,10 @@ impl Browser {
         self.inner.process.as_ref().map(process::Process::get_id)
     }
 
+    pub fn get_ws_url(&self) -> Url {
+        self.inner.process.as_ref().unwrap().debug_ws_url.clone()
+    }
+
     /// The tabs are behind an `Arc` and `Mutex` because they're accessible from multiple threads
     /// (including the one that handles incoming protocol events about new or changed tabs).
     pub fn get_tabs(&self) -> &Arc<Mutex<Vec<Arc<Tab>>>> {
